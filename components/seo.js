487bb0c90e07c1a4768cbb328f4ab48a99bb6c13
Abdul Rauf <rauf11.abdul@gmail.com>
2022-05-16 02:34:51 +0500

    final build

---
+++ components/seo.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+
+export default function Seo() {
+  const title = 'Codems | Code is poetry';
+  const description =
+    'Codems is an NFT collection of poems written in code. Each piece is released as an ERC-1155 token and lives on the Polygon blockchain.';
+
+  const metaImageUrl = '/meta-image.png';
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description} key='desc' />
+
+      {/* opengraph */}
+      <meta property='og:title' content={title} key='ogtitle' />
+      <meta property='og:description' content={description} key='ogdesc' />
+
+      <meta property='og:image' content={metaImageUrl} key='ogimage' />
+      <meta property='og:image:width' content='1920' />
+      <meta property='og:image:height' content='1004' />
+
+      {/* twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='twitter:title' content={title} />
+      <meta property='twitter:description' content={description} />
+      <meta property='twitter:image' content={metaImageUrl} />
+      <meta name='twitter:image:alt' content='Codems banner' />
+
+      {/* favicon */}
+      <link rel='shortcut icon' href='/favicon.png' />
+    </Head>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,38 +2,42 @@ import About from '../components/about';
 import Artist from '../components/artist';
 import Footer from '../components/footer';
 import Hero from '../components/hero';
+import Seo from '../components/seo';
 import ThirdEditionDrop from '../components/thirdEditionDrop';
 
 export default function Home() {
   return (
-    <main className='mx-auto px-4 sm:px-0 sm:w-[90%] lg:w-[83%]'>
-      <div className='my-10'>
-        <Hero />
-      </div>
+    <>
+      <Seo />
+      <main className='mx-auto px-4 sm:px-0 sm:w-[90%] lg:w-[83%]'>
+        <div className='my-10'>
+          <Hero />
+        </div>
 
-      <div className='my-32 md:my-48 xl:my-64'>
-        <About />
-      </div>
+        <div className='my-32 md:my-48 xl:my-64'>
+          <About />
+        </div>
 
-      <section
-        className='
+        <section
+          className='
         my-40 space-y-12
         md:space-y-20 md:my-64
         xl:space-y-32 xl:my-72'
-      >
-        <h2 className='ml-auto section-heading max-w-[40vw]'>
-          To mint, or not to mint.
-        </h2>
-        <ThirdEditionDrop />
-      </section>
+        >
+          <h2 className='ml-auto section-heading max-w-[40vw]'>
+            To mint, or not to mint.
+          </h2>
+          <ThirdEditionDrop />
+        </section>
 
-      <div className='my-48 md:my-64 xl:my-80'>
-        <Artist />
-      </div>
+        <div className='my-48 md:my-64 xl:my-80'>
+          <Artist />
+        </div>
 
-      <div className='pb-32 mt-64 md:mt-96'>
-        <Footer />
-      </div>
-    </main>
+        <div className='pb-32 mt-64 md:mt-96'>
+          <Footer />
+        </div>
+      </main>
+    </>
   );
 }
